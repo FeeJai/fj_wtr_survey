9dc36bc4d72248d1379f155df9303dfdcf3a8e25
Felix Jankowski <felix@jankowski.me>
2024-04-10 05:25:20 -0500

    Smooth animations on click

--- assets/js/chart-hook.js
+++ assets/js/chart-hook.js
@@ -4,35 +4,30 @@ export default {
   ...BaseChartHook,
   getConfig() {
     return {
-      type: 'bar',
+      type: "bar",
       data: {
         labels: ["You", this.el.dataset.participantName],
-        datasets: [{
-          label: 'Allocation Distribution',
-          data: [this.el.dataset.youValue, this.el.dataset.participantValue],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgb(255, 99, 132)',
-            'rgb(255, 159, 64)'
-          ],
-          borderWidth: 1
-        }]
+        datasets: [
+          {
+            label: "Allocation Distribution",
+            data: [this.el.dataset.youValue, this.el.dataset.participantValue],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: ["rgb(255, 99, 132)", "rgb(255, 159, 64)"],
+            borderWidth: 1,
+          },
+        ],
       },
       options: {
         scales: {
           y: {
             beginAtZero: true,
-            max: Math.round(this.el.dataset.yScaleMax)
-          }
-        }
-      }
+            max: Math.round(this.el.dataset.yScaleMax),
+          },
+        },
+      },
     };
-  }
-}
-
-
-
-
+  },
+};
